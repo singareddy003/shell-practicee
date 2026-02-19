dbbd1bb085563b41cf2e69093b6d31ddbbd8cf2a
Singareddy <singareddy003@gmail.com>
2026-02-19 08:29:41 +0530

    hello world started

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1:: hey $person2, how are you?"
+echo "$person2:: hello $person1, iam fine how you are?"
+echo "$person1:: i am fine too watsup?"
+echo "$person2:: nothing, just going to mars now, will you come?"
+echo "$person1:: sorry, you carry on! i will come once you back"
