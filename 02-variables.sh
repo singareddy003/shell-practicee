79e28f6af27a14fc685ae42c7c89303896c0544f
Singareddy <singareddy003@gmail.com>
2026-02-19 08:14:11 +0530

    hello world started

---
+++ 02-variables.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+person1=trump
+person2=mask
+
+echo "$person1:: hey $person2, how are you?"
+echo "$person2:: hello $person1, iam fine how you are?"
+echo "$person1:: i am fine too watsup?"
+echo "$person2:: nothing, just going to mars now, will you come?"
+echo "$person1:: sorry, you carry on! i will come once you back"
