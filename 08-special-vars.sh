44e139cfea00ac7cba89e132cc64e43b48b2636e
Singareddy <singareddy003@gmail.com>
2026-02-24 08:31:43 +0530

    hello world started

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -3,7 +3,7 @@
 echo "All variables passed to the script: $@" 
 echo "Number of variables: $#"
 echo "Script name: $0"
-echo "Current directory: $PWD
+echo "Current directory: $PWD"
 echo "user running this script: $USER"
 echo "Home directory of the user: $HOME"
 echo "PID of the script: $$"
