db937296ab91cfed009610e17f921a5759ce2a83
Singareddy <singareddy003@gmail.com>
2026-03-02 08:32:58 +0530

    colors

--- 11-functions.sh
+++ 11-functions.sh
@@ -9,7 +9,7 @@ then
 else 
     echo "you are running with root access"
 fi
-
+#validate function takes input as exit status , what command they tried to install 
     VALIDATE(){
     if [ $1 -eq 0 ]
 then 
