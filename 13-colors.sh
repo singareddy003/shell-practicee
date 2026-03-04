a3d7c2a75b97009f65474242055a9a457b740a3d
Singareddy <singareddy003@gmail.com>
2026-03-04 07:57:02 +0530

    colorss

---
+++ 13-colors.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]
+then 
+    echo -e "$R "Error:: Please run this script with root access $N"
+    exit 1 #give other than 0 upto 127
+else 
+    echo "you are running with root access"
+fi
+#validate function takes input as exit status , what command they tried to install 
+    VALIDATE(){
+    if [ $1 -eq 0 ]
+then 
+    echo -e "installing $2 is $G success: $N"
+else 
+    echo -e "installing $2 is $R failure $N"
+    exit 1
+ fi
+    }
+    dnf list installed mysql
+
+    if [ $? -ne 0 ]
+then 
+    echo "mysql is not installed .. going to install it"
+    dnf install mysql -y
+    VALIDATE $? ="mysql"
+else
+
+echo -e "nothing to do sql..$Y is already installed..$N "
+fi
+
+dnf list installed python3
+
+    if [ $? -ne 0 ]
+then 
+    echo "python3 is not installed .. going to install it"
+    dnf install python3 -y
+    VALIDATE $? ="python3"
+else
+
+echo -e "nothing to do python3 $Y is already installed.. $N"
+fi
+
+dnf list installed nginx
+
+    if [ $? -ne 0 ]
+then 
+    echo "nginx is not installed .. going to install it"
+    dnf install nginx -y
+    VALIDATE $? ="nginx"
+else
+
+echo -e "nginx is already installed.. nothing to d"
+fi
+
